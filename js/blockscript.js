ec23fc1f93867bfd662f32e422da0ca1f0c09a20
Florin1103 <florinboboc11@gmail.com>
2023-05-29 19:47:28 +0300

    Responsive to local storage login and sign up forms

--- js/blockscript.js
+++ js/blockscript.js
@@ -53,21 +53,73 @@ function validateForm() {
   }
   return true;
 }
-function showData() {
-    let peopleList;
-    if(localStorage.getItem("peopleList") == null) {
-        peopleList = [];
-    }
-    else {
-        peopleList = JSON.parse(localStorage.getItem("peopleList"))
-    }
-    let html = "";
-    peopleList.forEach(function(element, index){
-        html += "<tr>";
-        html += "<td>" + element.username + "</td>";
-        html += "<td>" + element.email + "</td>";
-        html += "<td>" + element.password + "</td>";
-        html += '<td><button onclick="deleteData('+index+')" class="btn">Delete</button><button onclick="updateData('+index+')" class="btn">Delete</button>';
-        html += "</tr>"
-    })
+// function showData() {
+//     let peopleList;
+//     if(localStorage.getItem("peopleList") == null) {
+//         peopleList = [];
+//     }
+//     else {
+//         peopleList = JSON.parse(localStorage.getItem("peopleList"))
+//     }
+//     let html = "";
+//     peopleList.forEach(function(element, index){
+//         html += "<tr>";
+//         html += "<td>" + element.username + "</td>";
+//         html += "<td>" + element.email + "</td>";
+//         html += "<td>" + element.password + "</td>";
+//         html += '<td><button onclick="deleteData('+index+')" class="btn">Delete</button><button onclick="updateData('+index+')" class="btn">Delete</button>';
+//         html += "</tr>"
+//     })
+// }
+// Retrieve account data from local storage
+let myAccount = JSON.parse(localStorage.getItem("myAccount"));
+
+// Check if the account data exists in local storage
+if (myAccount) {
+  // Account data exists, populate the login form with stored username and password
+  document.getElementById("username").value = myAccount.username;
+  document.getElementById("password").value = myAccount.password;
 }
+
+// Login button click event
+document.getElementById("login-button").addEventListener("click", function() {
+  // Get the entered username and password
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+
+  // Check if the entered username and password match the stored account data
+  if (myAccount && username === myAccount.username && password === myAccount.password) {
+    alert("Login successful!");
+    // Perform any login-related tasks here
+  } else {
+    alert("Invalid username or password!");
+    // Perform any actions for an invalid login attempt
+  }
+});
+
+// Sign up form submission event
+document.getElementById("signup-form").addEventListener("submit", function(event) {
+  event.preventDefault(); // Prevent the form from submitting
+
+  // Get the entered username, email, and password
+  let username = document.getElementById("username-signup").value;
+  let email = document.getElementById("email-signup").value;
+  let password = document.getElementById("password-signup").value;
+
+  // Create a new account object
+  let newAccount = {
+    username: username,
+    email: email,
+    password: password
+  };
+
+  // Store the new account data in local storage
+  localStorage.setItem("myAccount", JSON.stringify(newAccount));
+
+  alert("Account created successfully!");
+
+  // Perform any actions for a successful sign-up
+
+  // Reset the sign-up form
+  document.getElementById("signup-form").reset();
+});
