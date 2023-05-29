2ff09f59d121d17f6c75678fcee05e696120b669
Florin1103 <florinboboc11@gmail.com>
2023-05-29 20:06:51 +0300

    JS and CSS updates

--- js/blockscript.js
+++ js/blockscript.js
@@ -30,96 +30,63 @@ document.addEventListener("click", (event) => {
 };
 });
 // Local storage responsive buttons
-function validateForm() {
-  let username = document.getElementById("username-signup").value;
-  let email = document.getElementById("email-signup").value;
-  let password = document.getElementById("password-signup"); 
+// Retrieve accounts from local storage or initialize an empty array
+let accounts = JSON.parse(localStorage.getItem("accounts")) || [];
 
-  if(username == ""){
-    alert("Username is required");
-    return false;
-  }
-  if(email == ""){
-    alert("Email is required");
-    return false;
-  }
-  else if (!email.includes("@")){
-    alert("Invalid email address");
-    return false;
-  }
-  if(password = ""){
-    alert("Password is required");
-    return false;
-  }
-  return true;
-}
-// function showData() {
-//     let peopleList;
-//     if(localStorage.getItem("peopleList") == null) {
-//         peopleList = [];
-//     }
-//     else {
-//         peopleList = JSON.parse(localStorage.getItem("peopleList"))
-//     }
-//     let html = "";
-//     peopleList.forEach(function(element, index){
-//         html += "<tr>";
-//         html += "<td>" + element.username + "</td>";
-//         html += "<td>" + element.email + "</td>";
-//         html += "<td>" + element.password + "</td>";
-//         html += '<td><button onclick="deleteData('+index+')" class="btn">Delete</button><button onclick="updateData('+index+')" class="btn">Delete</button>';
-//         html += "</tr>"
-//     })
-// }
-// Retrieve account data from local storage
-let myAccount = JSON.parse(localStorage.getItem("myAccount"));
-
-// Check if the account data exists in local storage
-if (myAccount) {
-  // Account data exists, populate the login form with stored username and password
-  document.getElementById("username").value = myAccount.username;
-  document.getElementById("password").value = myAccount.password;
+if (accounts.length > 0) {
+  // Fill the login form with the first account in the array
+  document.getElementById("username").value = accounts[0].username;
+  document.getElementById("password").value = accounts[0].password;
 }
 
-// Login button click event
 document.getElementById("login-button").addEventListener("click", function() {
-  // Get the entered username and password
   let username = document.getElementById("username").value;
   let password = document.getElementById("password").value;
 
-  // Check if the entered username and password match the stored account data
-  if (myAccount && username === myAccount.username && password === myAccount.password) {
+  // Check if the entered username and password match any account
+  let matchedAccount = accounts.find(function(account) {
+    return account.username === username && account.password === password;
+  });
+
+  if (matchedAccount) {
     alert("Login successful!");
-    // Perform any login-related tasks here
   } else {
     alert("Invalid username or password!");
-    // Perform any actions for an invalid login attempt
   }
+
+  // Hide the login form
+  document.getElementById("login-form").style.display = "none";
 });
 
-// Sign up form submission event
 document.getElementById("signup-form").addEventListener("submit", function(event) {
-  event.preventDefault(); // Prevent the form from submitting
+  event.preventDefault();
 
-  // Get the entered username, email, and password
   let username = document.getElementById("username-signup").value;
   let email = document.getElementById("email-signup").value;
   let password = document.getElementById("password-signup").value;
 
-  // Create a new account object
   let newAccount = {
     username: username,
     email: email,
     password: password
   };
 
-  // Store the new account data in local storage
-  localStorage.setItem("myAccount", JSON.stringify(newAccount));
+  // Add the new account to the array
+  accounts.push(newAccount);
+
+  // Store the updated accounts array in local storage
+  localStorage.setItem("accounts", JSON.stringify(accounts));
 
   alert("Account created successfully!");
-
-  // Perform any actions for a successful sign-up
-
-  // Reset the sign-up form
   document.getElementById("signup-form").reset();
+
+  // Hide the sign-up form
+  document.getElementById("signup-form").style.display = "none";
 });
+
+
+// Year automatically update
+const yearSpan = document.getElementById('year');
+const currentYear = new Date().getFullYear();
+yearSpan.textContent = currentYear;
+
